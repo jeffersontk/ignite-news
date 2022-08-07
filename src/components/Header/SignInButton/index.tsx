695989034b5ba880dab4df468736be46066c6fe0
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-08-07 20:11:09 -0300

    add faunadb in project

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,21 +1,19 @@
 import styles from './styles.module.scss';
 import { FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
   const { data: session } = useSession()
-
-  console.log('session', session)
 
   return session ? (
     <button 
       type='button' 
       className={styles.signInButton}
-     
-    >
+      onClick={()=> signOut()}
+    > 
       <FaGithub color="#04d361"/>
-      Jefferson Brito
+       {session.user?.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,9 +11,46 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'read:user'
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({user, account, profile}){
+      const { email } = user
+   
+      try {
+        if(email) {
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                { data: { email } }
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            )
+          )  
+          return true
         }
+        return false
+      } catch(err) {
+        console.log(err)
+        return false
       }
-    })
-  ]
+    }
+  }
 })
